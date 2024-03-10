2f6e28c18e4762889dd7db1789e1befd968d9794
Saga690 <master.ayush.2003@gmail.com>
2024-03-10 20:07:29 +0530

    minor fixes

--- components/Canvas/Drone.js
+++ components/Canvas/Drone.js
@@ -2,37 +2,42 @@ import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from './Loader';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
+import { useLoader } from '@react-three/fiber'
+// import scene.gltf from 'public/buster_drone'
 
 const Drone = () => {
 
-    const drone = useGLTF('/public/buster_drone/scene.gltf');
+    // const drone = useGLTF('/public/buster_drone/scene.gltf');
+    // const drone = useLoader(GLTFLoader, scene.gltf)
+    const gltf = useLoader(GLTFLoader, '/../../public/buster_drone/scene.gltf')
 
     return (
         <mesh>
-            <hemisphereLight intensity={0.15} groundColor="black" />
+            {/* <hemisphereLight intensity={0.15} groundColor="black" />
             <pointLight intensity={1} />
             <primitive
                 object={drone.scene}
-            />
+            /> */}
         </mesh>
     )
 }
 
-const DroneCanvas = () => {
-    return (
-        <Canvas
-            frameLoop="demand"
-            shadows
-            camera={{ position: [20, 3, 5], fov: 25 }}
-            gl={{ preserveDrawingBuffer: true }}
-        >
-            <Suspense fallback={<CanvasLoader />}>
-                <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-                <Drone />
-            </Suspense>
-            <Preload all />
-        </Canvas>
-    )
-}
+// const DroneCanvas = () => {
+//     return (
+//         <Canvas
+//             frameLoop="demand"
+//             shadows
+//             camera={{ position: [20, 3, 5], fov: 25 }}
+//             gl={{ preserveDrawingBuffer: true }}
+//         >
+//             <Suspense fallback={<CanvasLoader />}>
+//                 <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+//                 <Drone />
+//             </Suspense>
+//             <Preload all />
+//         </Canvas>
+//     )
+// }
 
 export default Drone
